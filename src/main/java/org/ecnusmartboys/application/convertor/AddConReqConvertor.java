bc9729197c9d821360206291d2072f8541caa7dc
foliet <foliet@rainspace.cn>
2023-06-13 14:16:58 +0800

    fix: role

--- src/main/java/org/ecnusmartboys/application/convertor/AddConReqConvertor.java
+++ src/main/java/org/ecnusmartboys/application/convertor/AddConReqConvertor.java
@@ -4,9 +4,13 @@ import org.ecnusmartboys.application.dto.request.command.AddConsultantRequest;
 import org.ecnusmartboys.domain.model.user.Consultant;
 import org.ecnusmartboys.infrastructure.convertor.BaseConvertor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = BaseConvertor.COMPONENT_MODEL,
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AddConReqConvertor extends BaseConvertor<AddConsultantRequest, Consultant>{
+    @Override
+    @Mapping(target = "role", constant = Consultant.ROLE)
+    Consultant toEntity(AddConsultantRequest dto);
 }
